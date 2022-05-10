15dfbc0b3d376aca426904175dc1d71cf2b65530
Adiba1992 <105260561+Adiba1992@users.noreply.github.com>
2022-05-10 09:48:52 +0600

    Add files via upload

---
+++ Trajectory_plot.R
@@ -0,0 +1,138 @@
+library(monocle)
+library(Rtsne)
+library(dplyr)
+library(igraph)
+library(ggrepel)
+library(Biobase)
+library(BiocGenerics)
+library(cellrangerRkit)
+library(devtools)
+library(DDRTree)
+library(pheatmap)
+library(ggplot2)
+library(cowplot)
+library(clues)
+
+
+my_dir <- "E:/Adiba_SingleLung_Script_fig/my_thesis/imp_docu/ShenXine/Data/Lung_data"
+
+
+# load data
+gbm <- load_cellranger_matrix(my_dir)
+dim(exprs(gbm))
+
+my_cds <- newCellDataSet(exprs(gbm),
+                         phenoData = new("AnnotatedDataFrame", data = pData(gbm)),
+                         featureData = new("AnnotatedDataFrame", data = fData(gbm)),
+                         lowerDetectionLimit = 0.5,
+                         expressionFamily = negbinomial.size())
+
+# rename gene symbol column
+my_feat <- fData(gbm)
+names(my_feat) <- c('id', 'gene_short_name')
+
+# no warning
+my_cds <- newCellDataSet(exprs(gbm),
+                         phenoData = new("AnnotatedDataFrame", data = pData(gbm)),
+                         featureData = new("AnnotatedDataFrame", data = my_feat),
+                         lowerDetectionLimit = 0.8,
+                         expressionFamily = negbinomial.size())
+
+slotNames(my_cds)
+
+# normalisation and variance estimation steps
+my_cds <- estimateSizeFactors(my_cds)
+my_cds <- estimateDispersions(my_cds)
+
+##################Clustering cells without marker genes########################################################
+disp_table <- dispersionTable(my_cds)
+head(disp_table)
+
+
+
+#We will select genes, which have a mean expression >= 0.1, to use in the clustering step.
+table(disp_table$mean_expression>=0.1)
+
+unsup_clustering_genes <- subset(disp_table, mean_expression >= 0.1)
+
+my_cds <- setOrderingFilter(my_cds, unsup_clustering_genes$gene_id)
+
+plot_ordering_genes(my_cds)
+
+# if num_dim is not set, the default is set to 50
+# I'll use 5 here based on plot_pc_variance_explained
+#?reduceDimension
+my_cds <- reduceDimension(my_cds, max_components = 2, num_dim =10,
+                          reduction_method = 'tSNE', verbose = TRUE)
+
+
+# perform unsupervised clustering requesting 20 clusters
+my_cds <- clusterCells(my_cds, num_clusters = 12)
+
+#without specifying the number of clusters
+my_cds <- clusterCells(my_cds)
+
+# cluster information is in the Cluster column
+head(pData(my_cds))
+# store cluster info for comparing later
+my_cluster_dim_5 <- pData(my_cds)$Cluster
+
+plot_cell_clusters(my_cds)
+
+#I'll perform the differential expression analysis on the subset of genes where the mean expression (across cells) was >= 0.1 (see section "Clustering cells without marker genes").
+
+length(unsup_clustering_genes$gene_id)
+de_cluster_one <- differentialGeneTest(my_cds[unsup_clustering_genes$gene_id,],
+                                       fullModelFormulaStr = '~Cluster',
+                                       cores = 8)
+
+
+
+dim(de_cluster_one) 
+de_cluster_one %>% arrange(qval) %>% head()
+
+my_ordering_genes <- row.names(de_cluster_one)[order(de_cluster_one$qval<0.05)][1:1000]
+my_cds_subset <- setOrderingFilter(my_cds, ordering_genes = my_ordering_genes)
+my_cds_subset <- reduceDimension(my_cds_subset, method = 'DDRTree')
+
+my_cds_subset <- orderCells(my_cds_subset)
+plot_cell_trajectory(my_cds_subset, color_by = "Cluster")
+
+head(pData(my_cds_subset))
+
+my_pseudotime_de <- differentialGeneTest(my_cds_subset,
+                                         fullModelFormulaStr = "~sm.ns(Pseudotime)",
+                                         cores = 8)
+
+my_pseudotime_de %>% arrange(qval) %>% head()
+
+# save the top 6 genes
+my_pseudotime_de %>% arrange(qval) %>% head() %>% select(id) -> my_pseudotime_gene
+my_pseudotime_gene <- my_pseudotime_gene$id
+
+plot_genes_in_pseudotime(my_cds_subset[my_pseudotime_gene,])
+
+# cluster the top 50 genes that vary as a function of pseudotime
+my_pseudotime_de %>% arrange(qval<0.05) %>% head(1000) %>% select(id) -> gene_to_cluster
+gene_to_cluster <- gene_to_cluster$id
+
+my_pseudotime_cluster <- plot_pseudotime_heatmap(my_cds_subset[gene_to_cluster,],
+                                                 num_clusters = 7,
+                                                 cores = 8,
+                                                 show_rownames = TRUE,
+                                                 return_heatmap = TRUE)
+
+# hierarchical clustering was used to cluster the genes
+# we can cut the dendrogram to form the same 3 clusters as plot_pseudotime_heatmap
+my_cluster <- cutree(my_pseudotime_cluster$tree_row, 7)
+my_cluster
+
+# genes in cluster 1
+gene <- my_pseudotime_de[names(my_cluster[my_cluster ==1]),"gene_short_name"]
+
+clust_DE <- write.csv(gene, file = "C:/Users/HX-JTA/Desktop/fig/clustr1.csv")
+
+
+#Analyzing Branches in Single-Cell Trajectories
+plot_cell_trajectory(my_cds_subset, color_by = "Cluster")
+
---
+++ rCode.R
@@ -0,0 +1,167 @@
+library(dplyr)
+library(ggplot2)
+library(Seurat)
+
+
+newdat <- Read10X(data.dir = "C:/Users/ADIBA SULTANA/Desktop/Data_code/Data/")
+
+dim(newdat)
+summary(colSums(newdat))
+View(newdat)
+
+
+# Initialize the Seurat object with the raw (non-normalized data).
+pbmc <- CreateSeuratObject(counts = newdat, project = "pbmc3k", min.cells = 3, min.features = 200)
+
+pbmc
+
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+pbmc$nUMI <- Matrix::colSums(pbmc)
+
+slotNames(pbmc)
+head(pbmc@meta.data)
+View(pbmc@meta.data)
+summary(pbmc@meta.data)
+
+summary(pbmc)
+# Visualize QC metrics as a violin plot
+VlnPlot(pbmc, features = c("nFeature_RNA","nUMI", "percent.mt"), ncol = 3)
+dim(pbmc) #13580 x 1803
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+#pbmc <- NormalizeData(pbmc)
+
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(pbmc), 10)
+
+##########Scaling the data
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+
+##############Perform linear dimensional reduction#################
+
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+# Examine and visualize PCA results a few different ways
+print(pbmc[["pca"]], dims = 1:5, nfeatures = 5)
+
+
+################Determine the 'dimensionality' of the dataset#############
+# NOTE: This process can take a long time for big datasets, comment out for expediency. More
+# approximate techniques such as those implemented in ElbowPlot() can be used to reduce
+# computation time
+pbmc <- JackStraw(pbmc, num.replicate = 100)
+pbmc <- ScoreJackStraw(pbmc, dims = 1:20)
+JackStrawPlot(pbmc, dims = 1:15)
+ElbowPlot(pbmc)
+
+##################Cluster the cells#########################
+pbmc <- FindNeighbors(pbmc, dims = 1:10)
+pbmc <- FindClusters(pbmc, resolution = 0.5)
+
+# Look at cluster IDs of the first 5 cells
+head(Idents(pbmc), 5)
+
+
+# If you haven't installed UMAP, you can do so via reticulate::py_install(packages =
+# 'umap-learn')
+library(umap)
+pbmc <- RunUMAP(pbmc, dims = 1:10)
+
+# note that you can set `label = TRUE` or use the LabelClusters function to help label
+# individual clusters
+DimPlot(pbmc, reduction = "umap")
+
+#Run non-linear dimensional reduction (UMAP/tSNE)
+
+pbmc <- RunTSNE(object = pbmc, dims.use = 1:10, do.fast = TRUE)
+# note that you can set do.label=T to help label individual clusters
+DimPlot(object = pbmc, reduction = "tsne")
+
+#############Finding differentially expressed features (cluster biomarkers)######################
+
+# find all markers of cluster 1
+cluster0.markers <- FindMarkers(pbmc, ident.1 = 0, min.pct = 0.25)
+head(cluster0.markers, n = 10)
+print(x = head(x = cluster0.markers, n = 10))
+
+x0 <- c("ALAS2",  "AHSP",   "SLC25A37",   "MT-ND3", "HBA2", "HBB", "B2M", "HBA1", "HLA-B","SNCA")
+
+VlnPlot(pbmc, features = x0, slot = "counts", log = TRUE,pt.size = 0)
+
+######################8Cluster########################
+cluster1.markers <- FindMarkers(pbmc, ident.1 = 1, min.pct = 0.25)
+head(cluster1.markers, n = 10)
+x1 <- c("RP11-1143G9.4",  "MNDA",   "FCN1",   "VCAN", "CSTA", "AIF1", "CTSS", "LST1", "TYROBP","FGL2")
+VlnPlot(pbmc, features = x1, slot = "counts", log = TRUE,pt.size = 0)
+
+cluster2.markers <- FindMarkers(pbmc, ident.1 = 2, min.pct = 0.25)
+head(cluster2.markers, n = 10)
+x2 <- c("CD8B",  "GZMK",   "CMC1",   "CD8A", "CD2", "KLRD1", "CST7", "CD3E", "IL32","GZMA")
+VlnPlot(pbmc, features = x2, slot = "counts", log = TRUE,pt.size = 0)
+
+
+cluster3.markers <- FindMarkers(pbmc, ident.1 = 3, min.pct = 0.25)
+head(cluster3.markers, n = 10)
+x3 <- c("IL7R",  "LEF1",   "PRKCQ-AS1",   "MAL", "CCR7", "TCF7", "LTB", "LDHB", "NOSIP","CD27")
+VlnPlot(pbmc, features = x3, slot = "counts", log = TRUE,pt.size = 0)
+
+cluster4.markers <- FindMarkers(pbmc, ident.1 = 4, min.pct = 0.25)
+head(cluster4.markers, n = 10)
+x4 <- c("FGFBP2",  "ZNF683",   "GZMB",   "PRF1", "HOPX", "GZMH", "PATL2", "GNLY", "CST7","CTSW")
+VlnPlot(pbmc, features = x4, slot = "counts", log = TRUE,pt.size = 0)
+
+cluster5.markers <- FindMarkers(pbmc, ident.1 = 5, min.pct = 0.25)
+head(cluster5.markers, n = 10)
+x5 <- c("TUBB1",  "ITGA2B",   "C6orf25", "SH3BGRL2", "PRKAR2B", "C19orf33", "CMTM5", "GP9", "SPARC","SDPR")
+VlnPlot(pbmc, features = x5, slot = "counts", log = TRUE,pt.size = 0)
+
+cluster6.markers <- FindMarkers(pbmc, ident.1 =6, min.pct = 0.25)
+head(cluster6.markers, n = 10)
+x6 <- c("KLRC1",  "SH2D1B",   "KLRF1",   "KLRB1", "TMIGD2", "IL2RB", "TNFRSF18", "TXK", "PRSS23","PRF1")
+VlnPlot(pbmc, features = x6, slot = "counts", log = TRUE,pt.size = 0)
+
+cluster7.markers <- FindMarkers(pbmc, ident.1 = 7, min.pct = 0.25)
+head(cluster7.markers, n = 10)
+x7 <- c("VPREB3",  "MS4A1",   "CD79A",   "BANK1", "LINC00926", "RALGPS2", "CD22", "SPIB", "FCER2","HLA-DOB")
+VlnPlot(pbmc, features = x7, slot = "counts", log = TRUE,pt.size = 0)
+
+# find markers for every cluster compared to all remaining cells, report only the positive ones
+pbmc.markers <- FindAllMarkers(pbmc, min.pct = 0.25, logfc.threshold = 0.25)
+
+
+write.csv(pbmc.markers,"D:/hx_article/ppt/Lung/results/degLungSeurat.csv", row.names = FALSE)
+
+#DoHeatmap(pbmc, features = top5$gene) + NoLegend()
+dim(pbmc)
+top10 <- pbmc.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+DoHeatmap(pbmc, features = top10$gene, label = F) + NoLegend()
+DoHeatmap(pbmc, features = top10$gene, label = F)+ scale_fill_gradientn(colors = c("blue", "white", "red"))
+#DoHeatmap(pbmc)+ scale_fill_gradientn(colors = c("blue", "white", "red"))
+################VolcanoPlot################################
+res <- read.csv("D:/hx_article/ppt/Lung/results/degLungSeurat.csv")
+head(res)
+dim(res)
+
+
+with(res, plot(avg_logFC, -log10(p_val_adj), pch=20, main="Volcano plot", cex.axis=1.4))
+with(subset(res, p_val_adj<.01 & avg_logFC >2), points(avg_logFC, -log10(p_val_adj), pch=20, col="red", cex.axis=1.4))
+with(subset(res, p_val_adj<.01 & avg_logFC <(-2)), points(avg_logFC, -log10(p_val_adj), pch=20, col="green", cex.axis=1.4))
+
+
+DEg <- subset(res, p_val_adj <.01 & abs(avg_logFC)>=2)
+
+dim(DEgfdr2)
+
+Down <- subset(DEg, p_val_adj <.01 & (avg_logFC<(-2)))
+Up <- subset(DEg, p_val_adj <.01 & avg_logFC > 2)
+
+dim(Down)
+dim(Up)
+
+write.csv(DEg, file = "D:/hx_article/ppt/Lung/results/DEgfdr2.csv")
+write.csv(Down, file = "D:/hx_article/ppt/Lung/results/DownDEgfdr2.csv")
+write.csv(Up, file = "D:/hx_article/ppt/Lung/results/UpDEgfdr2.csv")
+
+
